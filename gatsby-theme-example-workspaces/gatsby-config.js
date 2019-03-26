76498fba7a71c3d385b0c20bd23047d255b61ee1
Brent Jackson <jxnblk@gmail.com>
2019-03-26 11:14:45 -0400

    Add placeholder page to theme

--- gatsby-theme-example-workspaces/gatsby-config.js
+++ gatsby-theme-example-workspaces/gatsby-config.js
@@ -1,5 +1,12 @@
+const path = require('path')
 module.exports = {
   plugins: [
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: path.join(__dirname, 'src/pages'),
+      }
+    },
     {
       resolve: 'gatsby-plugin-compile-es6-packages',
       options: {
---
+++ gatsby-theme-example-workspaces/src/pages/theme-page.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export default props =>
+  <h1>Theme Page</h1>
