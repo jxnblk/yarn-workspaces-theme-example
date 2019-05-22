dcb5e53d77b641c6eed8c3858a9746a78ec2c941
Brent Jackson <jxnblk@gmail.com>
2019-05-22 11:39:24 -0400

    Update example

--- gatsby-theme-example-workspaces/gatsby-config.js
+++ gatsby-theme-example-workspaces/gatsby-config.js
@@ -1,18 +1,11 @@
 const path = require('path')
+
 module.exports = {
   plugins: [
     {
       resolve: 'gatsby-plugin-page-creator',
       options: {
         path: path.join(__dirname, 'src/pages'),
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-compile-es6-packages',
-      options: {
-        modules: [
-          'gatsby-theme-example-workspaces',
-        ]
       }
     },
   ],
